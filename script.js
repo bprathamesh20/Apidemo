28fa670f100e176c20d29d4ae94b22355de74cd8
Prathamesh Bhandekar <98270118+bprathamesh20@users.noreply.github.com>
2022-01-23 15:00:30 +0000

    added show name basic

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 let form = document.querySelector('#form');
 let input = document.querySelector('#seachField');
 let searchButton = document.querySelector('button');
-
+let showContainer = document.querySelector('.showContainer')
 
 form.addEventListener('submit', async function (e) {
   e.preventDefault();
   const searchTerm = input.value;
   const res = await axios.get(`https://api.tvmaze.com/singlesearch/shows?q=${searchTerm}`);
     const img = document.createElement('img');
+    const showName = document.createElement('p');
     img.src = res.data.image.medium;
-    document.body.append(img);
+    showName.innerText = res.data.name;
+    showContainer.append(img);
+    showContainer.append(showName);
+    
+    console.log(res.data)
 });
 
 
