5733c4fe3d14363cf395d5f51f407c0250ac84e7
bprathamesh <>
2022-01-23 13:54:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let form = document.querySelector('#form');
+let userInput = document.querySelector('#seachField');
+let searchButton = document.querySelector('button');
+
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  console.dir(form)
+})
